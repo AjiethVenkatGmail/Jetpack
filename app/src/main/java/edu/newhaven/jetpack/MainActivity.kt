8136e90ba614431d525e1d7eb7f37088aca6d906
AjiethVenkat <AjiethVenkat@hotmail.com>
2021-03-27 17:16:00 -0400

    Navigation Menu Using Jetpack Libraries

--- app/src/main/java/edu/newhaven/jetpack/MainActivity.kt
+++ app/src/main/java/edu/newhaven/jetpack/MainActivity.kt
@@ -1,11 +1,38 @@
 package edu.newhaven.jetpack
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val storeFragment = StoreFragment()
+        val photosFragment = PhotosFragment()
+
+        setCurrentFragment(storeFragment)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+
+                R.id.storeFragment-> setCurrentFragment(storeFragment)
+
+                R.id.photosFragment-> setCurrentFragment(photosFragment)
+                }
+            true
+        }
     }
+
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frameContainer,fragment)
+            commit()
+        }
 }
